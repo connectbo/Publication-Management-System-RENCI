93c0f7c76bb00b88e029cad0534cb82b192f1847
Bo <bo7@live.unc.edu>
2019-06-21 16:32:44 -0400

    Update Project

--- server.js
+++ server.js
@@ -10,32 +10,56 @@ const router = express.Router();
 
 connectDB();
 
-const publication_schema = mongoose.Schema({ Title: Array, Authors: Array, DOI: String, Type:String });
+const publication_schema = mongoose.Schema({ Title: Array, Authors: Array, DOI: String, Type: String });
 const publication_model = mongoose.model('Publication', publication_schema)
 
+router.get('/reference/:id/:id2/save=yes', function (req, Res, next) {
+    
+})
+
 router.get('/reference/:id/:id2', function (req, Res, next) {
-    const apiUrl = 'https://api.crossref.org/v1/works/' + req.params.id + "/" + req.params.id2; // title DOI authors type citation
-    request.get(apiUrl, function (error, res, body) {
-        const parsedData = JSON.parse(body)['message'];
-        const parsedAuthors = JSON.parse(body)['message']['author'];
-        const fullnameAuthors = [];
-        for (i = 0; i<parsedAuthors.length; i++){
-            fullnameAuthors.push(parsedAuthors[i]['given']+" "+parsedAuthors[i]['family']);
+    const _DOI = req.params.id + "/" + req.params.id2;
+
+    publication_model.findOne({ DOI: _DOI }, function (err, publication) {
+
+        if (publication !== null) {
+            var pub = JSON.parse(JSON.stringify(publication));
+            pub["status"] = 'Found 1 matching result from RENCI Database';
+            Res.send(pub);
         }
-        const newData = new publication_model({ Title: parsedData['title'], Authors: fullnameAuthors, DOI: parsedData['DOI'], Type: parsedData['type']})
-        newData.save(function (err) {
-            if (err) {
-                throw err;
-            }
-            else{
-                Res.send(newData);
-                console.log('Publications added to MongoDB!');
-            }   
-        });
+        else {
+            const apiUrl = 'https://api.crossref.org/v1/works/' + _DOI; // title DOI authors type citation
+            request.get(apiUrl, function (error, res, body) {
+                const parsedData = JSON.parse(body)['message'];
+                const parsedAuthors = JSON.parse(body)['message']['author'];
+                const fullnameAuthors = [];
+                for (i = 0; i < parsedAuthors.length; i++) {
+                    fullnameAuthors.push(parsedAuthors[i]['given'] + " " + parsedAuthors[i]['family']);
+                }
+                let result = {
+                    Title: parsedData['title'], Authors: fullnameAuthors, DOI: parsedData['DOI'], Type: parsedData['type']
+                };
+                result['status'] = 'Found 1 matching result from Crossref API';
+                result['save'] = '0';
+                Res.send(result);
+
+                // const newData = new publication_model({ Title: parsedData['title'], Authors: fullnameAuthors, DOI: parsedData['DOI'], Type: parsedData['type'] })
+                // newData.save(function (err) {
+                //     if (err) {
+                //         throw err;
+                //     }
+                //     else {
+                //         console.log('Publications added to MongoDB!');
+                //         Res.send(newData);
+                //     }
+                // });
+            })
+        }
     });
+
 });
 
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
     res.send("Express API has been connected!");
 })
 
