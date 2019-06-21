886a219c6bb1689cdfe6464b1abc490b88075d2b
Matt Watson <mattwat@gmail.com>
2019-06-21 16:55:51 -0400

    log port to console

--- server.js
+++ server.js
@@ -70,4 +70,4 @@ app.use('/', router);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server is running!'));
+app.listen(PORT, () => console.log(`Server is running on port ${ PORT }!`));
