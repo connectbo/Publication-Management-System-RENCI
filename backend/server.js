2b37f3143e4b40910e71650ae159861c72c23e50
Bo <bo7@live.unc.edu>
2019-06-24 16:57:20 -0400

    Update Progress for Jun 24
    Change Repo Structure/ Add Routers for Browsing and Searching

---
+++ backend/server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const request = require('request');
+const connectDB = require('./config/db');
+const app = express();
+const cors = require('cors');
+
+
+const router = express.Router();
+
+connectDB();
+
+const publication_schema = mongoose.Schema({ Title: Array, Authors: Array, DOI: String, Type: String });
+const publication_model = mongoose.model('Publication', publication_schema)
+
+let newData;
+
+router.get('/reference/:id/:id2/save=yes', function (req, Res, next) {
+    newData.save(function (err) {
+        if (err) {
+            throw err;
+        }
+        else {
+            newData = JSON.parse(JSON.stringify(newData));
+            newData["status"] = 'Stored in RENCI Database'; 
+            Res.send(newData);
+        }
+    });
+})
+
+router.get('/reference/:id/:id2', function (req, Res, next) {
+    const _DOI = req.params.id + "/" + req.params.id2;
+
+    publication_model.findOne({ DOI: _DOI }, function (err, publication) {
+
+        if (publication !== null) {
+            var pub = JSON.parse(JSON.stringify(publication));
+            pub["status"] = 'Found 1 matching result from RENCI Database';
+            Res.send(pub);
+        }
+        else {
+            const apiUrl = 'https://api.crossref.org/v1/works/' + _DOI; // title DOI authors type citation
+            request.get(apiUrl, function (error, res, body) {
+                const parsedData = JSON.parse(body)['message'];
+                const parsedAuthors = JSON.parse(body)['message']['author'];
+                const fullnameAuthors = [];
+                for (i = 0; i < parsedAuthors.length; i++) {
+                    fullnameAuthors.push(parsedAuthors[i]['given'] + " " + parsedAuthors[i]['family']);
+                }
+                let result = {
+                    Title: parsedData['title'], Authors: fullnameAuthors, DOI: parsedData['DOI'], Type: parsedData['type']
+                };
+                result['status'] = 'Found 1 matching result from Crossref API';
+                result['save'] = '0';
+                Res.send(result);
+                newData = new publication_model({ Title: parsedData['title'], Authors: fullnameAuthors, DOI: parsedData['DOI'], Type: parsedData['type'] })
+            })
+        }
+    });
+
+});
+
+router.get('/', function(req, res, next){
+    publication_model.find({}, function(err, pubs){
+        if(err){
+            console.log(err);
+            throw(err);
+        }
+        res.send(pubs);
+    })
+})
+
+app.use(cors());
+
+app.use('/', router);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server is running!'));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import AppBar from '@material-ui/core/AppBar';
@@ -20,6 +21,7 @@ import './App.css';
 
 const useStyles = makeStyles({
   card: {
+    margin: '10px 5px',
     minWidth: 300,
   },
   title: {
@@ -41,10 +43,74 @@ const useStyles = makeStyles({
   input: {
     marginLeft: 8,
     flex: 1
+  },
+  barTitle:{
+    flexGrow: 1,
   }
 });
 
 function App() {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge='start' color="inherit" aria-label="Menu">
+          </IconButton>
+          <Typography href="/" className={classes.barTitle}>
+            <strong>RENCI</strong> Publication Management System
+          </Typography>
+          <Button href="/search" color="inherit">Search</Button>
+        </Toolbar>
+      </AppBar>
+      <Router>
+        <div>
+          <Route path="/search" component={Search} />
+          <Route path="/" exact component={Home} />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+function Home() {
+  const classes = useStyles();
+  const [pubs, setPubState] = useState('');
+  //const [pubAuthor, setpubAuthor] = useState('');
+
+  const getPubs = async () => {
+    const PubResult = await fetch(`http://localhost:5000`)
+      .then(res => res.json());
+    setPubState(PubResult);
+  }
+
+  useEffect(getPubs, []);
+
+  const pubArray = [];
+  Object.keys(pubs).forEach(function(key){
+    pubArray.push(pubs[key]);
+  })
+  console.log(pubs);
+  console.log(typeof pubs);
+
+  return (
+    <Container>
+      {
+        pubArray.map(pub => <Card className={classes.card}>
+          <CardContent>
+            <Typography className={classes.body}><strong>Title: </strong> {pub.Title}</Typography>
+            <Typography className={classes.body}><strong>DOI: </strong>{pub.DOI}</Typography>
+            <Typography className={classes.body}><strong>Author(s): </strong>{pub.Authors.join(", ")}</Typography>
+            <Typography className={classes.body}><strong>Type: </strong> {pub.Type}</Typography>
+          </CardContent>
+        </Card>)
+      }
+      </Container>
+  );
+}
+
+function Search() {
+  console.log("Search visited!");
   const classes = useStyles();
   const [ref, setrefState] = useState('');
   const [result, setResultState] = useState('');
@@ -66,23 +132,14 @@ function App() {
 
   const savePub = event => {
     fetch(`http://localhost:5000/reference/${ref}/save=yes`)
-    .then(res => res.json())
-    .then(data => {
-      setResultState(data);
-    });
+      .then(res => res.json())
+      .then(data => {
+        setResultState(data);
+      });
   }
 
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge='start' color="inherit" aria-label="Menu">
-          </IconButton>
-          <Typography>
-            <strong>RENCI</strong> Publication Management System
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <Container className={classes.root}>
         <Typography><strong>PubFinder</strong></Typography>
         <InputBase className={classes.input} id="ref" type="text" placeholder="Enter DOI to start searching.." value={ref} onChange={handleChange}></InputBase>
@@ -99,7 +156,7 @@ function App() {
             <Typography className={classes.body}><strong>Type: </strong> {result.Type}</Typography>
           </CardContent>
           <CardActions>
-          {result.save ? <Button onClick={savePub} size="small" color="secondary">Save it</Button> : null}
+            {result.save ? <Button onClick={savePub} size="small" color="secondary">Save it</Button> : null}
           </CardActions>
         </Card>
       </Container>
