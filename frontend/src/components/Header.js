d83a18ea26b0836dc7dd1d5de71598b1ab46649b
Bo <peterbozhou@gmail.com>
2019-06-27 09:40:12 -0400

    Separate Header Component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+    barTitle: {
+        flex: 1,
+        textDecoration: 'none',
+        color: 'white',
+    }
+});
+
+function Header() {
+    const classes = useStyles();
+    return (
+        <AppBar position="static">
+            <Toolbar variant="dense">
+                <IconButton edge='start' color="inherit" aria-label="Menu">
+                </IconButton>
+                <a href="/" color="inherit" className={classes.barTitle}>
+                    <strong>RENCI </strong> Publication Management System
+          </a>
+                <Button href="/" color="inherit">Home</Button>
+                <Button href="/search" color="inherit">Search</Button>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header;
