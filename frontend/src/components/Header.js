e260f2ecd3bb1730e15884d668c7412f04b23af1
Bo <peterbozhou@gmail.com>
2019-07-19 15:21:26 -0400

    Make header Button with Navlink

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button'
-import { NavLink } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
     barTitle: {
