6e214b15a8f68494638fb323dd0b7074eb4715e2
Bo <peterbozhou@gmail.com>
2019-10-21 15:41:58 -0400

    Test beta ready

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -45,6 +45,7 @@ function Search() {
   const [categories, setCategories] = useState([]);
   const currentUrl = window.location.hostname;
   let categoryString = "";
+  let toReport = {};
 
   async function fetchCategories() {
     const categoryResults = await fetch(`http://${currentUrl}:5000/category`)
@@ -52,7 +53,7 @@ function Search() {
     setCategories(categoryResults);
   }
 
-  useEffect(() => {fetchCategories();}, []);
+  useEffect(() => { fetchCategories(); }, []);
 
   const [categoryJSON, setcategoryJSON] = useState({});
   const [categoryArray, setcategoryArray] = useState([]);
@@ -104,6 +105,7 @@ function Search() {
   }
 
   const handleSubmit = event => {
+    let toReportString = "Found results: ";
     if (ref !== '') {
       fetch(`http://${currentUrl}:5000/reference/${ref}`)
         .then(res => res.json())
@@ -119,47 +121,61 @@ function Search() {
         .then(res => res.json())
         .then(data => {
           setpubArrayState(data);
+          data.forEach(pub => {
+            let curr_type = pub.Type;
+            if (toReport[curr_type]) {
+              toReport[curr_type] += 1;
+            }
+            else {
+              toReport[curr_type] = 1;
+            }
+          })
+          Object.keys(toReport).forEach(key => {
+            toReportString += `${key}: ${toReport[key]} `;
+          })
+          console.log(toReportString);
+          setStatusState(toReportString);
         })
-    }
   }
-  return (
-    <div>
-      <Container className={classes.root}>
-        <Typography><strong>DOI</strong></Typography><Input className={classes.input} id="ref" type="text" value={ref} onChange={handlerefChange}></Input>
-        <Typography className={classes.input}><strong>Title</strong></Typography><Input className={classes.input} id="title" type="text" value={title} onChange={handleTitleChange}></Input>
-        <Typography className={classes.input}><strong>Author</strong></Typography><Input className={classes.input} id="author" type="text" value={author} onChange={handleAuthorChange}></Input>
-        <FormControl className={classes.input}>
-          <FormLabel><strong>Type</strong></FormLabel>
-          <FormGroup>
-            {categoryArray.map(cate => <FormControlLabel control={<Checkbox checked={categoryJSON[cate]} onChange={handleCategoryChange} value={cate} />} label={cate} ></FormControlLabel>)}
-          </FormGroup>
-        </FormControl>
-        <FormControl>
-          <TextField className={classes.input} id="sdate" label="Start Date" type="date" value={sdate} onChange={handle_sdate_Change} InputLabelProps={{
-            shrink: true,
-          }} ></TextField>
-          <TextField className={classes.input} id="edate" label="End Date" type="date" value={edate} onChange={handle_edate_Change} InputLabelProps={{
-            shrink: true,
-          }}></TextField>
-        </FormControl>
-        <Button className={classes.subButton} variant="contained" color="secondary" onClick={handleSubmit}>
-          Search </Button>
-      </Container>
-      <Typography className={classes.body}><strong>{status}</strong></Typography>
-      <Container>
-        {pubArray.map(pub => <Card className={classes.card}>
-          <CardContent>
-            <Typography className={classes.body}><strong>Title: </strong> {pub.Title}</Typography>
-            <Typography className={classes.body}><strong>DOI: </strong><a href={"https://dx.doi.org/" + pub.DOI}>{pub.DOI}</a></Typography>
-            <Typography className={classes.body}><strong>Author(s): </strong>{pub.Authors.join(", ")}</Typography>
-            <Typography className={classes.body}><strong>Created Date: </strong>{pub.Created_Date}</Typography>
-            <Typography className={classes.body}><strong>Type: </strong>{pub.Type}</Typography>
-          </CardContent>
-        </Card>)
-        }
-      </Container>
-    </div>
-  );
+}
+return (
+  <div>
+    <Container className={classes.root}>
+      <Typography><strong>DOI</strong></Typography><Input className={classes.input} id="ref" type="text" value={ref} onChange={handlerefChange}></Input>
+      <Typography className={classes.input}><strong>Title</strong></Typography><Input className={classes.input} id="title" type="text" value={title} onChange={handleTitleChange}></Input>
+      <Typography className={classes.input}><strong>Author</strong></Typography><Input className={classes.input} id="author" type="text" value={author} onChange={handleAuthorChange}></Input>
+      <FormControl className={classes.input}>
+        <FormLabel><strong>Type</strong></FormLabel>
+        <FormGroup>
+          {categoryArray.map(cate => <FormControlLabel control={<Checkbox checked={categoryJSON[cate]} onChange={handleCategoryChange} value={cate} />} label={cate} ></FormControlLabel>)}
+        </FormGroup>
+      </FormControl>
+      <FormControl>
+        <TextField className={classes.input} id="sdate" label="Start Date" type="date" value={sdate} onChange={handle_sdate_Change} InputLabelProps={{
+          shrink: true,
+        }} ></TextField>
+        <TextField className={classes.input} id="edate" label="End Date" type="date" value={edate} onChange={handle_edate_Change} InputLabelProps={{
+          shrink: true,
+        }}></TextField>
+      </FormControl>
+      <Button className={classes.subButton} variant="contained" color="secondary" onClick={handleSubmit}>
+        Search </Button>
+    </Container>
+    <Typography className={classes.body}><strong>{status}</strong></Typography>
+    <Container>
+      {pubArray.map(pub => <Card className={classes.card}>
+        <CardContent>
+          <Typography className={classes.body}><strong>Title: </strong> {pub.Title}</Typography>
+          <Typography className={classes.body}><strong>DOI: </strong><a href={"https://dx.doi.org/" + pub.DOI}>{pub.DOI}</a></Typography>
+          <Typography className={classes.body}><strong>Author(s): </strong>{pub.Authors.join(", ")}</Typography>
+          <Typography className={classes.body}><strong>Created Date: </strong>{pub.Created_Date}</Typography>
+          <Typography className={classes.body}><strong>Type: </strong>{pub.Type}</Typography>
+        </CardContent>
+      </Card>)
+      }
+    </Container>
+  </div>
+);
 }
 
 export default Search;
