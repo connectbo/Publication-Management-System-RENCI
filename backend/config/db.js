555dcc55a681ceb5d05e625b5cd0f21819f50f72
Bo <peterbozhou@gmail.com>
2020-03-13 15:59:44 -0400

    Changed to MongoDB in Docker

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 require('dotenv').config({ path: '/Users/Peter/Documents/GitHub/Publication-Management-System-RENCI/.env'});
 const mongoose = require('mongoose');
 const db = {
-    mongoURI: process.env.MONGO_URI || 'localhost',
+    mongoURI: 'mongodb://root:example@mongodb:27017/test?authSource=admin' || process.env.MONGO_URI,
 };
 const connectDB = async() => {
     try{
         await mongoose.connect(db.mongoURI, {
             useNewUrlParser: true
         });
-        console.log('MongoDB Cloud Connected...');
+        console.log('MongoDB Connected...');
     } catch(err) {
         console.log(err.message);
         process.exit(1);
