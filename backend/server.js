11a7bf55af9e48eb4a0a57feec25c23043e7d7c1
Bo Zhou <bo7@ad.unc.edu>
2021-01-25 11:32:20 -0500

    Update Docker Compose

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/', PubRouter);
+app.use('/*', PubRouter);
 
 const API_PORT = process.env.API_PORT || 5050;
 
